38449693b6baefc2c6d39490cfe47ecf111da57f
Moe Sy <moe@sy.dev>
2025-07-17 16:41:53 -0700

    fix: conditional price based on discount

--- exercises/010_if2.zig
+++ exercises/010_if2.zig
@@ -1,16 +1,10 @@
-//
-// If statements are also valid expressions:
-//
-//     const foo: u8 = if (a) 2 else 3;
-//
+// Use expressions at variable assignment.
 const std = @import("std");
 
 pub fn main() void {
     const discount = true;
 
-    // Please use an if...else expression to set "price".
-    // If discount is true, the price should be $17, otherwise $20:
-    const price: u8 = if ???;
+    const price: u8 = if (discount) 17 else 20;
 
     std.debug.print("With the discount, the price is ${}.\n", .{price});
 }
